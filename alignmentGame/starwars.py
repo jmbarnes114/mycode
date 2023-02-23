358f5a32be5f2569a693efcc4cc33ddffe725364
jmbarnes114 <Jon.Barnes@tlgcohort.com>
2023-02-23 18:37:29 +0000

    Changed the code and fixed errors

--- alignmentGame/starwars.py
+++ alignmentGame/starwars.py
@@ -3,51 +3,74 @@
 print("Welcome to the D&D alignment game! Please answer the following questions to determine your Star Wars character's alignment.")
 
 # initialize variables
+import random
+
+# Define the questions to determine the player's alignment
+questions = [
+    "Do you believe in following rules and traditions, even if they're inconvenient? (y/n) ",
+    "Do you prioritize helping others, even at your own expense? (y/n) ",
+    "Do you value honesty and integrity above all else? (y/n) ",
+    "Do you believe that might makes right? (y/n) ",
+    "Do you prefer to work alone, or as part of a team? (alone/team) "
+]
+
+# Define the possible alignments and their descriptions
+alignments = {
+    ("Lawful", "Good"): "The Noble Hero, a true Jedi and Light side user, Obi-Wan Kenobi",
+    ("Lawful", "Neutral"): "The Lawful Guardian The fearless Bounty Hunter, Bobba Fett",
+    ("Lawful", "Evil"): "The Tyrannical Ruler, you rule with an iron fist, Lord Vader",
+    ("Neutral", "Good"): "The Carefree Adventurer, once a jedi, you turned away to find yourself, Ashoka Tano",
+    ("Neutral", "Neutral"): "The Neutral Observer, as a droid, you dont have much say regarding alligiance, C3PO",
+    ("Neutral", "Evil"): "The torn one, you are torn between the dark side and light side of the force, but you are a dark user, Kylo Ren",
+    ("Chaotic", "Good"): "The Rebel with a Cause, Never out of malice or hate, you will go anywhere and work with anyone, Han Solo",
+    ("Chaotic", "Neutral"): "The selfish one, once a jedi, turned sides based on your own beliefs, Count Dooku",
+    ("Chaotic", "Evil"): "The Chaotic Villain, The evil Master of the dark side of the force, Empoer Palpatine"
+}
+
+# Initialize the variables to track the player's alignment
 lawful = False
 chaotic = False
 good = False
 evil = False
 
-# ask questions to determine character's alignment
-while True:
-    answer = input("Do you believe in following rules and traditions, even if they're inconvenient? (y/n) ")
-    if answer.lower() == "y":
-        lawful = True
-        break
-    elif answer.lower() == "n":
-        chaotic = True
-        break
-    else:
-        print("Invalid answer. Please enter 'y' or 'n'.")
+# Loop through the questions to determine the player's alignment
+for question in questions:
+    while True:
+        answer = input(question)
+        if answer.lower() == "y":
+            lawful = True
+            break
+        elif answer.lower() == "n":
+            chaotic = True
+            break
+        elif answer.lower() == "alone":
+            evil = True
+            break
+        elif answer.lower() == "team":
+            good = True
+            break
+        else:
+            print("Invalid answer. Please enter 'y' or 'n', 'alone', or 'team'.")
 
-while True:
-    answer = input("Do you prioritize helping others, even at your own expense? (y/n) ")
-    if answer.lower() == "y":
-        good = True
-        break
-    elif answer.lower() == "n":
-        evil = True
-        break
-    else:
-        print("Invalid answer. Please enter 'y' or 'n'.")
-
-# determine character's alignment based on answers
-if lawful and good:
-    print("You are Lawful Good! A true Jedi Obi-Wan Kenobi")
-elif lawful and evil:
-    print("You are Lawful Evil! The fearsome, Lord Vader")
-elif chaotic and good:
-    print("You are Chaotic Good! Never tell me the odds, Han Solo")
-elif chaotic and evil:
-    print("You are Chaotic Evil! Corrupted by the dark side, Emperor palpatine")
-elif lawful:
-    print("You are Lawful Neutral. Now get your bounty Bobba Fett")
+# Determine the player's alignment based on their answers
+alignment = []
+if lawful:
+    alignment.append("Lawful")
 elif chaotic:
-    print("You are Chaotic Neutral. Once a Jedi, turned to another side because of your beliefs, Count Dooku")
-elif good:
-    print("You are Neutral Good. Once a jedi, but ultimately turned away, Ashoka Tano")
-elif evil:
-    print("You are Neutral Evil. You are torn between the force but rest on the dark side, Kylo Ren")
+    alignment.append("Chaotic")
 else:
-    print("You are True Neutral. A droid who doesnt have much say regarding alliances, C3PO")
+    alignment.append("Neutral")
+
+if good:
+    alignment.append("Good")
+elif evil:
+    alignment.append("Evil")
+else:
+    alignment.append("Neutral")
+
+# Print a fun description of the player's alignment
+if tuple(alignment) in alignments:
+    print("Congratulations, you are " + alignments[tuple(alignment)] + "!")
+else:
+    print("You are " + " ".join(alignment) + ". That's...interesting. Lets hope you agree and thanks for playimg!")
 
